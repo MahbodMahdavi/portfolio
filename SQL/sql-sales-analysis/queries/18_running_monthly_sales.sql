a176da7a047b1436c4442a2e90d4bb85b1adddca
Mahbod Mahdavi <github.com.tactless799@passmail.com>
2026-07-25 18:15:08 -0400

    Update 18_running_monthly_sales.sql

--- SQL/sql-sales-analysis/queries/18_running_monthly_sales.sql
+++ SQL/sql-sales-analysis/queries/18_running_monthly_sales.sql
@@ -1,20 +1,39 @@
+/*
+Query: Running Monthly Sales
+
+Purpose:
+Calculate cumulative sales over time.
+
+Author: Mahbod Mahdavi
+*/
+
 USE SalesAnalysis;
 GO
 
-WITH MonthlySales AS (
+WITH MonthlySales AS
+(
     SELECT
-        DATEFROMPARTS(YEAR(Order_Date), MONTH(Order_Date), 1) AS MonthDate,
-        CAST(SUM(Sales) AS DECIMAL(10,2)) AS MonthlySales
+        YEAR(Order_Date) AS SalesYear,
+        MONTH(Order_Date) AS SalesMonthNumber,
+        DATENAME(MONTH, Order_Date) AS SalesMonth,
+        SUM(Sales) AS MonthlySales
     FROM train
     GROUP BY
-        DATEFROMPARTS(YEAR(Order_Date), MONTH(Order_Date), 1)
+        YEAR(Order_Date),
+        MONTH(Order_Date),
+        DATENAME(MONTH, Order_Date)
 )
+
 SELECT
-    FORMAT(MonthDate, 'yyyy-MM') AS [Month],
-    MonthlySales,
+    SalesYear,
+    SalesMonth,
+    CAST(MonthlySales AS DECIMAL(10,2)) AS MonthlySales,
     CAST(
-        SUM(MonthlySales) OVER (ORDER BY MonthDate)
-        AS DECIMAL(10,2)
+        SUM(MonthlySales) OVER (
+            ORDER BY SalesYear, SalesMonthNumber
+        ) AS DECIMAL(10,2)
     ) AS RunningTotalSales
 FROM MonthlySales
-ORDER BY MonthDate;
+ORDER BY
+    SalesYear,
+    SalesMonthNumber;
