803bdc9b3d4b9a345317d9c6d45db698ccc75491
Mahbod Mahdavi <github.com.tactless799@passmail.com>
2026-07-25 18:15:24 -0400

    Update 19_top_customer_per_region.sql

--- SQL/sql-sales-analysis/queries/19_top_customer_per_region.sql
+++ SQL/sql-sales-analysis/queries/19_top_customer_per_region.sql
@@ -1,31 +1,44 @@
+/*
+Query: Top Customer per Region
+
+Purpose:
+Identify the highest-selling customer in each region.
+
+Author: Mahbod Mahdavi
+*/
+
 USE SalesAnalysis;
 GO
 
-WITH CustomerSales AS (
+WITH CustomerSales AS
+(
     SELECT
         Region,
         Customer_Name,
-        CAST(SUM(Sales) AS DECIMAL(10,2)) AS TotalSales
+        SUM(Sales) AS TotalSales
     FROM train
     GROUP BY
         Region,
         Customer_Name
 ),
-RankedCustomers AS (
+RankedCustomers AS
+(
     SELECT
         Region,
         Customer_Name,
         TotalSales,
-        ROW_NUMBER() OVER (
+        ROW_NUMBER() OVER
+        (
             PARTITION BY Region
             ORDER BY TotalSales DESC
-        ) AS RowNum
+        ) AS CustomerRank
     FROM CustomerSales
 )
+
 SELECT
     Region,
     Customer_Name,
-    TotalSales
+    CAST(TotalSales AS DECIMAL(10,2)) AS TotalSales
 FROM RankedCustomers
-WHERE RowNum = 1
+WHERE CustomerRank = 1
 ORDER BY Region;
