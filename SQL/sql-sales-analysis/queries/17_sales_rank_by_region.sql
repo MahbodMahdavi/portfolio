f6dcebc3748ffa6336437a21b818785fc125950e
Mahbod Mahdavi <github.com.tactless799@passmail.com>
2026-07-25 18:14:49 -0400

    Update 17_sales_rank_by_region.sql

--- SQL/sql-sales-analysis/queries/17_sales_rank_by_region.sql
+++ SQL/sql-sales-analysis/queries/17_sales_rank_by_region.sql
@@ -1,10 +1,21 @@
+/*
+Query: Sales Rank by Region
+
+Purpose:
+Rank regions based on total sales.
+
+Author: Mahbod Mahdavi
+*/
+
 USE SalesAnalysis;
 GO
 
 SELECT
     Region,
     CAST(SUM(Sales) AS DECIMAL(10,2)) AS TotalSales,
-    RANK() OVER (ORDER BY SUM(Sales) DESC) AS SalesRank
+    RANK() OVER (
+        ORDER BY SUM(Sales) DESC
+    ) AS SalesRank
 FROM train
 GROUP BY Region
 ORDER BY SalesRank;
